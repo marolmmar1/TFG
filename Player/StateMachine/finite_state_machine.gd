c2ff9aeafb2c56d0930d874c15caf66221252e6b
marolmmar1 <marolmmar1@alum.us.es>
2024-09-17 19:05:23 +0200

    finished basic state machine and moveState

--- Player/StateMachine/finite_state_machine.gd
+++ Player/StateMachine/finite_state_machine.gd
@@ -1,14 +1,20 @@
 class_name FiniteStateMachine
 extends Node
 
+@export_category("Provided")
+@export var player: CharacterBody3D
+
+@export_category("Main")
 @export var state: State
+
+@export_category("DEBUG")
+
 
 func _ready():
 	change_state(state)
 
 func change_state(new_state: State):
 	if state is State:
-		state._exit_state()
-	new_state._enter_estate()
+		state.exit_state(state)
+	new_state.enter_state(new_state)
 	state = new_state
-

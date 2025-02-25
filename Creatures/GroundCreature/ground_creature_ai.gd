4a0670de8a54ea51a1bf0f0ab803502ab229bdac
Me <me@dontemailme.com>
2025-02-25 02:14:22 +0100

    astar edge weights

--- Creatures/GroundCreature/ground_creature_ai.gd
+++ Creatures/GroundCreature/ground_creature_ai.gd
@@ -10,12 +10,22 @@ var path_index = 0
 #DEBUG
 var target
 
+var walk_speed
+var climb_speed
+var crawl_speed
+
+var calculating_astar = false
+
 
 func init(_astar, _controller, target):
 	self.astar_graph = _astar
 	self.controller = _controller
 	#DEBUG
 	self.target = target
+
+	self.walk_speed = controller.find_child("Walk").speed
+	self.climb_speed = controller.find_child("Climb").speed
+	self.crawl_speed = controller.find_child("Crawl").speed
 
 
 func get_closest_node(point: Vector2, threshold: float):
@@ -38,10 +48,12 @@ class AstarAINode:
 
 	var node: Vector2i
 	var parent: AstarAINode
-	var cost: int
+	var cost: float
 
 
 func astar(current_node: Vector2i, target_node: Vector2i) -> Array:
+	calculating_astar = true
+
 	var pending = []
 	var explored = []
 
@@ -52,37 +64,49 @@ func astar(current_node: Vector2i, target_node: Vector2i) -> Array:
 	while pending.size() > 0: #TODO prevent infinite loop and split process through ticks
 
 		# #DEBUG
-		# await get_tree().create_timer(15.25).timeout
+		await get_tree().create_timer(1.5).timeout
 		astar_graph.queue_redraw()
 
 		var lowest_cost_node = null
 		var lowest_cost = INF
 
+		# Get pending node with lowest calculated cost + heuristic (expected cost)
 		for node in pending:
-			var cost = node_cost(node, AstarAINode.new(target_node, null, 0))
+			var cost = node.cost + node_heuristic(node, AstarAINode.new(target_node, null, 0))
 			if cost < lowest_cost:
 				lowest_cost = cost
 				lowest_cost_node = node
 
+		# We're exploring this one
 		pending.erase(lowest_cost_node)
 		explored.append(lowest_cost_node)
 
+		# Goal reached
 		if lowest_cost_node.node == target_node:
 			return build_path(start_node, lowest_cost_node)
 
+		# #DEBUG
+		astar_graph.astar_on_going.append(astar_graph.tmhelper.to_world_position(lowest_cost_node.node))
+		print("cost: ", lowest_cost_node.cost)
+		print("heuristic: ", node_heuristic(lowest_cost_node, AstarAINode.new(target_node, null, 0)))
+		print("total: ", lowest_cost_node.cost + node_heuristic(lowest_cost_node, AstarAINode.new(target_node, null, 0)))
+
 		var neighbors = get_neighbors(lowest_cost_node.node)
 		for neighbor in neighbors:
+			# Skip explored neighbors
 			if explored.filter(func(x): return x.node == neighbor).size() > 0:
 				continue
 
 			var pending_neighbor = pending.filter(func(x): return x.node == neighbor).pop_front()
 
+			# If not pending add the neighbor
 			if not pending_neighbor:
 				pending_neighbor = AstarAINode.new(neighbor, lowest_cost_node, 0)
 				var cost = node_cost(lowest_cost_node, pending_neighbor)
 				pending_neighbor.cost = cost
 				pending.append(pending_neighbor)
 				
+			# If pending update neighbor cost if lower
 			else:
 				var cost = node_cost(lowest_cost_node, pending_neighbor)
 				if cost < pending_neighbor.cost:
@@ -91,24 +115,45 @@ func astar(current_node: Vector2i, target_node: Vector2i) -> Array:
 
 	return []
 
-func node_cost(from: AstarAINode, to: AstarAINode) -> int:
-	var heuristic = controller.position.distance_to(astar_graph.tmhelper.to_world_position(to.node))
-	var movement_cost = 0
-	if from.parent:
-		movement_cost = from.cost
-		
-	return heuristic + movement_cost
-
 func get_neighbors(node: Vector2i) -> Array:
 	var neighbors = []
 
-	# #DEBUG
-	# astar_graph.astar_on_going.append(astar_graph.tmhelper.to_world_position(node))
-	
 	for edge in astar_graph.astar_nodes[node]:
 		neighbors.append(edge.to)
 
 	return neighbors
+
+
+# Calculate costs
+func node_heuristic(from: AstarAINode, to: AstarAINode) -> float:
+	return astar_graph.tmhelper.to_world_position(from.node).distance_to(astar_graph.tmhelper.to_world_position(to.node))
+
+
+func node_cost(from: AstarAINode, to: AstarAINode) -> float:
+
+	var parent_cost = 0
+	if from.parent:
+		parent_cost = from.cost
+
+	var movement_cost = 0
+	for edge in astar_graph.astar_nodes[from.node]:
+		if edge.to == to.node:
+			movement_cost = speed_cost(edge)
+		
+	return movement_cost + parent_cost
+
+func speed_cost(edge: Edge) -> float:
+	var dist = edge.from.distance_to(edge.to)
+	# v = d / t, t = d / v
+	if edge.movement_type == Edge.MovementType.WALK:
+		return dist / walk_speed
+	elif edge.movement_type == Edge.MovementType.CLIMB:
+		return dist / climb_speed
+	elif edge.movement_type == Edge.MovementType.CRAWL:
+		return dist / crawl_speed
+	
+	return 1
+
 
 func build_path(start_node: AstarAINode, end_node: AstarAINode) -> Array:
 	var _path = []
@@ -126,13 +171,13 @@ func build_path(start_node: AstarAINode, end_node: AstarAINode) -> Array:
 
 #TODO pass to higher AI
 func tick():
-	if not path:
+	if not path and not calculating_astar:
 		var current_node = get_closest_node(controller.position, 1000)
 		var target_node = get_closest_node(target.position, 1000)
 
 		# #DEBUG
-		# astar_graph.astar_target = astar_graph.tmhelper.to_world_position(target_node)
-		# astar_graph.queue_redraw()
+		astar_graph.astar_target = astar_graph.tmhelper.to_world_position(target_node)
+		astar_graph.queue_redraw()
 
 		if current_node == null or target_node == null:
 			return
